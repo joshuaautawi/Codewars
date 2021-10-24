ca629f9e2352208191028cc1934a2165b96356be
joshuaautawi <autawijoshua@gmail.com>
2021-10-24 18:49:27 +0700

    update

--- kyu6/4. Create Phone Number.js
+++ kyu6/4. Create Phone Number.js
@@ -2,5 +2,3 @@ function createPhoneNumber(numbers){
     numbers = numbers.join("")
     return `(${numbers.slice(0,3)}) ${numbers.slice(3,6)}-${numbers.slice(6)}`
 }
-
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
